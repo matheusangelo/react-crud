fc946221a63c5c9d640715abd753be3cc7de0006
Matheus Silva <msilva@brasilseg.com.br>
2019-05-29 17:17:25 -0300

    Inclusão do modal

--- src/components/painel/Painel.jsx
+++ src/components/painel/Painel.jsx
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import { Container, Table, Row, Col, Button, Alert } from 'reactstrap';
-import {IoIosCheckmark, IoIosClose, IoIosBuild} from 'react-icons/io';
+import { Container, Table, Row, Col, Button, Alert, Modal, ModalHeader,ModalBody, ModalFooter } from 'reactstrap';
+import { IoIosCheckmark, IoIosClose, IoIosBuild } from 'react-icons/io';
 
 let mock = [
     {
-        "id":1,
+        "id": 1,
         "nome_produto": "XPTO",
         "descricao": "Carga de um produto qualquer",
-        "data_criacao":"10/02/2018"
+        "data_criacao": "10/02/2018"
     },
     {
-        "id":1,
+        "id": 1,
         "nome_produto": "XPTO",
         "descricao": "Carga de um produto qualquer",
-        "data_criacao":"10/02/2018"
+        "data_criacao": "10/02/2018"
     },
     {
-        "id":1,
+        "id": 1,
         "nome_produto": "XPTO",
         "descricao": "Carga de um produto qualquer",
-        "data_criacao":"10/02/2018"
+        "data_criacao": "10/02/2018"
     }
 ]
 
@@ -28,15 +28,22 @@ class Painel extends Component {
         super(props);
         this.state = {
             titulo: "Pineapple",
-            produtos: mock
+            produtos: mock,
+            modalEditar: false,
         }
+    }
+
+    toggleEditar = () => {
+        this.setState({
+            modalEditar: !this.state.modalEditar,
+        });
     }
 
     render() {
         return (
             <Container fluid>
                 <Row className="mt-2 ml-2">
-                    <Button color="primary"> Novo produto</Button>
+                    <Button color="primary" onClick={this.toggleEditar}> Novo produto</Button>
                 </Row>
                 <Row>
                     <Col>
@@ -61,14 +68,14 @@ class Painel extends Component {
                                 </tr>
                             </thead>
                             <tbody>
-                                {this.state.produtos.map((produto,i)=>{
-                                    return(
-                                        <tr key ={i}>
+                                {this.state.produtos.map((produto, i) => {
+                                    return (
+                                        <tr key={i}>
                                             <td>{produto.id}</td>
                                             <td>{produto.nome_produto}</td>
                                             <td>{produto.descricao}</td>
                                             <td>{produto.data_criacao}</td>
-                                            <td><IoIosCheckmark/><IoIosClose/><IoIosBuild/></td>
+                                            <td><IoIosCheckmark /><IoIosClose /><IoIosBuild /></td>
                                         </tr>
                                     )
                                 })}
@@ -76,6 +83,18 @@ class Painel extends Component {
                         </Table>
                     </Col>
                 </Row>
+                <Modal isOpen={this.state.modalEditar} toggle={this.toggleEditar}>
+                    <ModalHeader toggle={this.toggle}>
+                        Produto
+                    </ModalHeader>
+                    <ModalBody>
+
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="secondary" onClick={this.toggleEditar}>Cancelar</Button>
+                        <Button color="success" onClick={this.vincularSinistros}>Cadastrar</Button>
+                    </ModalFooter>
+                </Modal>
             </Container>
         )
     }
