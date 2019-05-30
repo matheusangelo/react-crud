4f4ec8e75a034d83c142a0b14d8cf117bc7da6a3
Matheus Silva <msilva@brasilseg.com.br>
2019-05-30 13:20:28 -0300

    Modificações form login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Container, Form, Input,Button} from 'reactstrap';
+import {Link} from 'react-router-dom'
 
 class Login extends Component{
     constructor(props){
@@ -9,11 +10,12 @@ class Login extends Component{
         return(
             <Container className="container-login">
                 <Form className="form-login">
-                    <h1>Pineapple</h1>
+                    <h1 className="titulo-login">Pineapple</h1>
                     <Input type="text" placeholder="Nome Usuário"/>
                     <Input type="password" placeholder="Senha"/>
                     <hr />
-                    <Button>Entrar</Button>
+                    <Button className="mb-2">Entrar</Button>
+                    <Link><span>Criar conta grátis</span></Link>
                 </Form>
             </Container>
         )
