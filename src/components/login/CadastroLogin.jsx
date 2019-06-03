939f60059b64bdc1c20f2f1f00261ea06fe6553f
Matheus Silva <msilva@brasilseg.com.br>
2019-06-03 10:22:49 -0300

    Ajuste

--- src/components/login/CadastroLogin.jsx
+++ src/components/login/CadastroLogin.jsx
@@ -1,25 +1,27 @@
-import React, {Component} from 'react';
-import {Container, Form, Input,Button} from 'reactstrap';
-import {Link} from 'react-router-dom'
+import React, { Component } from 'react';
+import { Container, Form, Input, Button } from 'reactstrap';
+import { Link } from 'react-router-dom'
 
-class CadastrarLogin extends Component{
-    constructor(props){
+class CadastrarLogin extends Component {
+    constructor(props) {
         super(props);
     }
-    render(){
-        return(
-            <Container className="container-login">
-                <Form className="form-login">
-                    <h1 className="titulo-login">Cadastro</h1>
-                    <Input type="text" placeholder="Nome Usuário" required/>
-                    <Input type="email" placeholder="E-mail" required/>
-                    <Input type="password" placeholder="Senha" required/>
-                    <Input type="password" placeholder="Confirmar Senha" required/>
-                    <hr />
-                    <Button className="mb-2">Cadastrar-se</Button>
-                    <Link to='/' className="btn btn-primary">Voltar</Link>
-                </Form>
-            </Container>
+    render() {
+        return (
+            <div className="mt-5">
+                <Container className="container-login">
+                    <Form className="form-login">
+                        <h1 className="titulo-login">Cadastro</h1>
+                        <Input type="text" placeholder="Nome Usuário" required />
+                        <Input type="email" placeholder="E-mail" required />
+                        <Input type="password" placeholder="Senha" required />
+                        <Input type="password" placeholder="Confirmar Senha" required />
+                        <hr />
+                        <Button className="mb-2">Cadastrar-se</Button>
+                        <Link to='/' className="btn btn-primary">Voltar</Link>
+                    </Form>
+                </Container>
+            </div>
         )
     }
 }
